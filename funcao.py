bea049380afa936d76cee2bba490d2105159aeb0
Katrisbug <bonetkatrina@gmail.com>
2025-10-07 16:07:59 -0300

    funcao interagir_no_zoologico e teste

---
+++ funcao.py
@@ -0,0 +1,18 @@
+from classe import *
+
+def interagir_no_zoologico(animais: list):
+    for animal in animais:
+        print(animal.descrever())
+        print(animal.emitir_som())
+        if isinstance(animal, IVoador):
+            print(animal.voar())
+        if isinstance(animal, INadador):
+            print(animal.nadar())
+        print()
+
+#------------------------------------------------------
+leao = Leao("Simba", 5)
+pinguim = Pinguim("Tux", 3)
+aguia = Aguia("Altair", 4)
+animais = [leao, pinguim, aguia]
+interagir_no_zoologico(animais)
